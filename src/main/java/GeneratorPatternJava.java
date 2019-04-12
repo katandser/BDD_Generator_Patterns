6fb53f06cd6a561969f66635c8990bb622480d9c
katandser <katandser96@mail.ru>
2019-04-12 15:26:51 +0700

    All scenario success

--- src/main/java/GeneratorPatternJava.java
+++ src/main/java/GeneratorPatternJava.java
@@ -12,33 +12,17 @@ public class GeneratorPatternJava {
                 "} ";
     }
     public String createSingleton(String nameOfClass) {
-        if (nameOfClass.equals("Singleton")) {
-            return this.createSingleton();
-        }
-        else if (nameOfClass.equals("YES")) {
-            return " class YES {\n" +
-                    "    private static YES instance;\n" +
-                    "    private YES() {}\n" +
-                    "    public static YES getInstance() {\n" +
-                    "        if (instance == null) {\n" +
-                    "            instance = new YES();\n" +
-                    "        }\n" +
-                    "        return instance;\n" +
-                    "    }\n" +
-                    "} ";
-        }
-        else {
-            return " class A {\n" +
-                    "    private static A instance;\n" +
-                    "    private A() {}\n" +
-                    "    public static A getInstance() {\n" +
-                    "        if (instance == null) {\n" +
-                    "            instance = new A();\n" +
-                    "        }\n" +
-                    "        return instance;\n" +
-                    "    }\n" +
-                    "} ";
-        }
+        return " class " + nameOfClass + " {\n" +
+                "    private static " + nameOfClass + " instance;\n" +
+                "    private " + nameOfClass + "() {}\n" +
+                "    public static " + nameOfClass + " getInstance() {\n" +
+                "        if (instance == null) {\n" +
+                "            instance = new " + nameOfClass + "();\n" +
+                "        }\n" +
+                "        return instance;\n" +
+                "    }\n" +
+                "} ";
+
     }
 
 
