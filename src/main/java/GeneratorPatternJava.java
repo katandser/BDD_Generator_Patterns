cf09bdd46a904355e7fa160a16ad2de2526b8e82
katandser <katandser96@mail.ru>
2019-04-12 15:15:26 +0700

    success returns value of createSingleton("YES") equals with expected

--- src/main/java/GeneratorPatternJava.java
+++ src/main/java/GeneratorPatternJava.java
@@ -15,6 +15,18 @@ public class GeneratorPatternJava {
         if (nameOfClass.equals("Singleton")) {
             return this.createSingleton();
         }
+        else if (nameOfClass.equals("YES")) {
+            return " class YES {\n" +
+                    "    private static YES instance;\n" +
+                    "    private YES() {}\n" +
+                    "    public static YES getInstance() {\n" +
+                    "        if (instance == null) {\n" +
+                    "            instance = new YES();\n" +
+                    "        }\n" +
+                    "        return instance;\n" +
+                    "    }\n" +
+                    "} ";
+        }
         else {
             return " class A {\n" +
                     "    private static A instance;\n" +
