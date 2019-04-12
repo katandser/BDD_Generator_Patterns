be9221da428cde017f38d4f3973316fc08b1ff24
katandser <katandser96@mail.ru>
2019-04-12 14:48:18 +0700

    success createSingleton return right value

--- src/main/java/GeneratorPatternJava.java
+++ src/main/java/GeneratorPatternJava.java
@@ -1,6 +1,15 @@
 public class GeneratorPatternJava {
     public String createSingleton() {
-        return "";
+        return " class Singleton {\n" +
+                "    private static Singleton instance;\n" +
+                "    private Singleton() {}\n" +
+                "    public static Singleton getInstance() {\n" +
+                "        if (instance == null) {\n" +
+                "            instance = new Singleton();\n" +
+                "        }\n" +
+                "        return instance;\n" +
+                "    }\n" +
+                "} ";
     }
 
 }
